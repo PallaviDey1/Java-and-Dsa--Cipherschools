33bfbe100011852ac802562d9612752ce188740d
Pallavi <103404379+PallaviDey1@users.noreply.github.com>
2023-06-18 04:12:35 +0530

    Add files via upload

---
+++ Primitivedtyp.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class primitivedtyp {
+	public static void main(String[]args) {
+		int x = 5;
+		int y = 4;
+		System.out.println("x is : " + x);
+		System.out.println("y is : " + y);
+		byte b = 10;
+		System.out.println("b is : "+ b);
+		byte b2 = (byte)129;
+		System.out.println("b2 is : "+ b2);
+		int i = b;
+		System.out.println("i is : "+i);
+	}
+}
