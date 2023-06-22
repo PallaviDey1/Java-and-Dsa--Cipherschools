455aea2f4519a83348552f81ab7e8d652f888085
Pallavi <103404379+PallaviDey1@users.noreply.github.com>
2023-06-23 01:38:21 +0530

    Add files via upload

---
+++ Lecture 5 -Cipherschools/src/corejava/pmt.java
@@ -0,0 +1,45 @@
+package corejava;
+
+public class pmt {
+
+
+	public static void main(String[]args) {
+		int x = 5;
+		int y = 4;
+		System.out.println("x is : " + x);
+		System.out.println("y is : " + y);
+		byte b = 10;
+		System.out.println("b is : "+ b);
+		int i = b;
+		System.out.println("i is : " +i);
+		byte b2 = (byte)129;
+		System.out.println("b2 is : "+ b2);
+		byte b3 = (byte)128;
+		System.out.println("b3 is : "+ b3);
+		byte b4 = (byte)256;
+		System.out.println("b4 is : "+ b4);
+		float f = (float)5.5;//by default considers decimal to be double
+		float f2 = 5.5f;
+		double d = 10.5;
+		char ch = 'a';
+		System.out.println(ch);
+		//Different  ways to convert char to int
+		int z = x + y;
+		//+ is operator and x,y are operands
+		
+		
+		
+		
+		//Increment and decrement
+		int m = 5;
+		int n = m++;
+		System.out.println("m is : "+m+" , n is : " +n);
+		int p =5;
+		int q = ++p;
+		System.out.println("p is :"+p+" , q is : "+q);
+		
+	}
+}
+
+
+
