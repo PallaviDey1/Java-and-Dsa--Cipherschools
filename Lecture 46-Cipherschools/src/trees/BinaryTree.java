a17287cd8695b3872c7ccfcb1593bab146cb51b9
Pallavi <103404379+PallaviDey1@users.noreply.github.com>
2023-08-09 20:59:11 +0530

    Add files via upload

---
+++ Lecture 46-Cipherschools/src/trees/BinaryTree.java
@@ -0,0 +1,41 @@
+package trees;
+class Node{
+	int data;
+	Node left;
+	Node right;
+	Node(int data)
+	{
+		this.data=data;
+		this.left=null;
+		this.right=null;
+	}
+}
+
+public class BinaryTree {
+	Node root;
+	BinaryTree(int data)
+	{
+		this.root=new Node(data);
+	}
+	int treeSum(Node root)
+	{
+		if(root==null)return 0;
+		return root.data+treeSum(root.left)+treeSum(root.right);
+	}
+	int countNodes(Node root)
+	{
+		if(root==null)return 0;
+		return 1+countNodes(root.left)+countNodes(root.right);
+	}
+	public static void main(String[]args) {
+		BinaryTree bt= new BinaryTree(2);
+		bt.root.left=new Node(3);
+		bt.root.right=new Node(5);
+		bt.root.left.right=new Node(9);
+		bt.root.right.left=new Node(7);
+		System.out.println(bt.treeSum(bt.root));
+		System.out.println(bt.countNodes(bt.root));
+		
+	}
+
+}
